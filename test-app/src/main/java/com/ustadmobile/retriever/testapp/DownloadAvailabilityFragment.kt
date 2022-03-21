0b8c1ddc44f243a3c9b6b833730d2319c3b6aea0
Mike Dawson <mike@ustadmobile.com>
2022-03-21 20:41:18 +0400

    Implement usage of notification for download on test app.

--- test-app/src/main/java/com/ustadmobile/retriever/testapp/DownloadAvailabilityFragment.kt
+++ test-app/src/main/java/com/ustadmobile/retriever/testapp/DownloadAvailabilityFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.work.*
@@ -69,6 +70,8 @@ class DownloadAvailabilityFragment: Fragment(), DIAware, OnAvailabilityChanged {
                     .build()
                 WorkManager.getInstance(requireContext()).enqueueUniqueWork(urls.first(), ExistingWorkPolicy.KEEP,
                     workRequest)
+
+                findNavController().popBackStack(R.id.local_dest, false)
             }
         }.root
     }
--- test-app/src/main/java/com/ustadmobile/retriever/testapp/work/DownloadWorker.kt
+++ test-app/src/main/java/com/ustadmobile/retriever/testapp/work/DownloadWorker.kt
@@ -1,13 +1,21 @@
 package com.ustadmobile.retriever.testapp.work
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import androidx.work.CoroutineWorker
+import androidx.work.ForegroundInfo
+import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import com.ustadmobile.retriever.Retriever
 import com.ustadmobile.retriever.RetrieverRequest
 import com.ustadmobile.retriever.fetcher.RetrieverProgressEvent
 import com.ustadmobile.retriever.fetcher.RetrieverProgressListener
+import com.ustadmobile.retriever.testapp.R
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 import org.kodein.di.android.closestDI
@@ -23,12 +31,60 @@ class DownloadWorker(
 
     private val retriever: Retriever by instance()
 
-    override suspend fun onRetrieverProgress(retrieverProgressEvent: RetrieverProgressEvent) {
+    private lateinit var notificationBuilder: NotificationCompat.Builder
 
+    private var notificationId = 0L
+
+    private val notificationManager = ContextCompat.getSystemService(
+        applicationContext,
+        NotificationManager::class.java
+    ) as NotificationManager
+
+    private fun createNotificationBuilder(downloadUrl: String) :NotificationCompat.Builder{
+        notificationId = System.currentTimeMillis()
+
+        val intent = WorkManager.getInstance(applicationContext)
+            .createCancelPendingIntent(id)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createChannel()
+        }
+
+        return NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID)
+            .setContentTitle(downloadUrl)
+            .setOngoing(true)
+            .setAutoCancel(true)
+            .setSmallIcon(R.drawable.ic_file_download_white_24dp)
+            .addAction(android.R.drawable.ic_delete, applicationContext.getString(R.string.cancel), intent)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createChannel() {
+        val mNotificationChannel = NotificationChannel(NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_ID, NotificationManager.IMPORTANCE_DEFAULT)
+
+        mNotificationChannel.vibrationPattern = longArrayOf(0)
+        mNotificationChannel.enableVibration(false)
+        mNotificationChannel.setSound(null, null)
+
+        notificationManager.createNotificationChannel(mNotificationChannel)
+    }
+
+
+    override fun isRunInForeground(): Boolean {
+        return true
+    }
+
+    override suspend fun onRetrieverProgress(retrieverProgressEvent: RetrieverProgressEvent) {
+        notificationBuilder.setProgress(retrieverProgressEvent.totalBytes.toInt(),
+            retrieverProgressEvent.bytesSoFar.toInt(), retrieverProgressEvent.totalBytes == 0L)
+
+        setForegroundAsync(ForegroundInfo(notificationId.hashCode(), notificationBuilder.build()))
     }
 
     override suspend fun doWork(): Result {
         val downloadUrl = inputData.getString(KEY_URL) ?: throw IllegalArgumentException("No url!")
+        notificationBuilder = createNotificationBuilder(downloadUrl)
         val downloadDir = File(applicationContext.dataDir, "downloads")
         downloadDir.takeIf { !it.exists() }?.mkdirs()
         val fileName = downloadUrl.substringAfterLast("/")
@@ -41,6 +97,8 @@ class DownloadWorker(
 
     companion object {
         const val KEY_URL = "url"
+
+        const val NOTIFICATION_CHANNEL_ID = "RETRIEVER_NOTIFICATION_CHANNEL_ID"
     }
 
 }
